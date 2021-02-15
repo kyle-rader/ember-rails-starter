344b6ae48caba1af09327999046f77ff248d3894
Kyle Rader <kylewrader@gmail.com>
2021-02-15 10:37:06 -0800

    Setup Tailwind CSS

---
+++ client/app/tailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
