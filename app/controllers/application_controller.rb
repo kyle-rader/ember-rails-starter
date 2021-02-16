e84e4685b8eff5e6c7fff757d1c41fe8cbd6d1d0
Kyle Rader <kylewrader@gmail.com>
2021-02-15 18:19:08 -0800

    Go back to render file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def fallback_index_html
-        send_file "#{Rails.root}/public/index.html"
+        render file: "#{Rails.root}/public/index.html"
     end
 end
