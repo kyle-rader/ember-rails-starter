86e172bae30a4d0956b5b81ae778ae4fca1176be
Kyle Rader <kylewrader@gmail.com>
2021-02-15 17:58:45 -0800

    Use send_file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def fallback_index_html
-        render file: Rails.root.join('public/index.html')
+        send_file "#{Rails.root}/public/index.html"
     end
 end
